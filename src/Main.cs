e2626842be12d061494a2bb36aa71d2b332ea41a
Contiinuum <68207985+Contiinuum@users.noreply.github.com>
2020-08-05 14:19:41 +0200

    Add files via upload

--- src/Main.cs
+++ src/Main.cs
@@ -24,7 +24,7 @@ namespace AudicaModding
         public static class BuildInfo
         {
             public const string Name = "TrippyMenus";  // Name of the Mod.  (MUST BE SET)
-            public const string Author = "Alternity"; // Author of the Mod.  (Set as null if none)
+            public const string Author = "Continuum"; // Author of the Mod.  (Set as null if none)
             public const string Company = null; // Company that made the Mod.  (Set as null if none)
             public const string Version = "0.1.0"; // Version of the Mod.  (MUST BE SET)
             public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
